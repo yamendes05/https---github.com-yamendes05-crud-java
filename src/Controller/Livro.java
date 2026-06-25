987578e2f0aabb49b0f94c4567b1fc1454322c8d
yamendes05 <yara.mendes@cs.cruzeirodosul.edu.br>
2026-06-25 11:10:12 -0300

    primeiras classes

--- src/Controller/Livro.java
+++ src/Controller/Livro.java
@@ -1,5 +1,8 @@
 package Controller;
 
 public class Livro {
-
+    public String Nome;
+    public int id;
+    public String autor;
+    
 }
--- src/Controller/Usuario.java
+++ src/Controller/Usuario.java
@@ -1,5 +1,7 @@
 package Controller;
 
 public class Usuario {
-
+    public String Nome;
+    public int id;
+    public int idade;
 }
